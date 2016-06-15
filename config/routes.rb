1d4090f7ad3b353f7108a8c984d5d6840b6f9f6e
thecookieorg <marko@thecookie.org>
2016-06-15 15:20:10 -0700

    merchant dashboard added, bootstrap-sass added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
+
+  get 'pages/index'
+
   devise_for :merchants
+
+  authenticated :merchant do
+    root 'dashboards#index', as: :authenticated_root
+  end
+
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
